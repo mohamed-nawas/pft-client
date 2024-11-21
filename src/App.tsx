6e3d3b91f953d8d0e4421c0e4c4552359d78e07d
mohamed-nawas <mgmnawas@gmail.com>
2024-11-21 13:45:13 +0530

    added log to see env base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import React from "react";
 import Home from "@pages/Home";
 import PrivateRoute from "@routes/PrivateRoute";
+import { API } from "@services/api";
 
 
 export default function App() {
+
+  React.useEffect(() => {
+    console.log('BACKEND URL => ' + API.defaults.baseURL)
+  }, [])
+
   return <PrivateRoute component={Home} />
 }
